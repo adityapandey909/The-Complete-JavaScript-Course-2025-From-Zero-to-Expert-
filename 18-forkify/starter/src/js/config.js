187ba08d8936e33d78cdbc55b6c2522b44a4a105
Aditya Pandey <PANDEYADITYA44517@GMAIL.COM>
2026-06-12 18:10:01 +0530

    Completed Forkify App

---
+++ 18-forkify/starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '444737f5-13d7-4c4a-965f-461ef32ec60a';
+export const MODAL_CLOSE_SEC = 2.5;
